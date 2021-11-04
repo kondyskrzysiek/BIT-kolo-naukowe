29ec27ed57f4ad075b61cf39b24b061b32ef204d
Krzysiek <krzysiekkondys@gmail.com>
2021-11-04 18:29:12 +0100

    ćwiczenie 10
    Zmodyfikuj kalkulator z zadania 3 tak, aby wyrażenia dało się wprowadzać w jednej linii.
    Wskazówka: napisy (stringi) posiadają metodę .split(), która dzieli string na listę słów (elementów oddzielonych w stringu spacjami).
    
    Przykład:
    > 2 + 3
    The sum of numbers is: 5
    
    > 2 ** 4
    The power of numbers is: 26

---
+++ ćwiczenia 1/ćwiczenia/10.py
@@ -0,0 +1,17 @@
+while True:
+    if __name__ == '__main__':
+        lista =[]
+        string = input('<<napisz działanie ze spacjami pomiędzy cyframi a operatorem>>\n')
+        for i in string.split(" "):
+            lista.append(i)
+
+        if lista[1] == '+':
+            print(float(lista[0]) + float(lista[2]))
+        elif lista[1] == '-':
+            print(float(lista[0]) - float(lista[2]))
+        elif lista[1] == '*':
+            print(float(lista[0]) * float(lista[2]))
+        elif lista[1] == '/':
+            print(float(lista[0]) / float(lista[2]))
+        elif lista[1] == '^':
+            print(float(lista[0]) ** float(lista[2]))
