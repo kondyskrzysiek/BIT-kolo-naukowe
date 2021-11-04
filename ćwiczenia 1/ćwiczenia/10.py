528e9d79ceb040f60fe209b0a9186d850f368692
Krzysiek <krzysiekkondys@gmail.com>
2021-11-04 18:56:48 +0100

    ćwiczenie 10 poprawienie
    poprawienie na jedno pętle a nie pętle nieskończoną ,, while True: ,, na początku

--- ćwiczenia 1/ćwiczenia/10.py
+++ ćwiczenia 1/ćwiczenia/10.py
@@ -1,17 +1,16 @@
-while True:
-    if __name__ == '__main__':
-        lista =[]
-        string = input('<<napisz działanie ze spacjami pomiędzy cyframi a operatorem>>\n')
-        for i in string.split(" "):
-            lista.append(i)
+if __name__ == '__main__':
+    lista =[]
+    string = input('<<napisz działanie ze spacjami pomiędzy cyframi a operatorem>>\n')
+    for i in string.split(" "):
+        lista.append(i)
 
-        if lista[1] == '+':
-            print(float(lista[0]) + float(lista[2]))
-        elif lista[1] == '-':
-            print(float(lista[0]) - float(lista[2]))
-        elif lista[1] == '*':
-            print(float(lista[0]) * float(lista[2]))
-        elif lista[1] == '/':
-            print(float(lista[0]) / float(lista[2]))
-        elif lista[1] == '^':
-            print(float(lista[0]) ** float(lista[2]))
+    if lista[1] == '+':
+        print(float(lista[0]) + float(lista[2]))
+    elif lista[1] == '-':
+        print(float(lista[0]) - float(lista[2]))
+    elif lista[1] == '*':
+        print(float(lista[0]) * float(lista[2]))
+    elif lista[1] == '/':
+        print(float(lista[0]) / float(lista[2]))
+    elif lista[1] == '^':
+        print(float(lista[0]) ** float(lista[2]))
