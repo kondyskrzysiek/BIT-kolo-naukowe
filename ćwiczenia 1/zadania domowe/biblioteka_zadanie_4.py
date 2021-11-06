d27ee999f72fd1d7b7be409fa28916ae7089e6e6
Krzysiek <krzysiekkondys@gmail.com>
2021-11-06 19:34:15 +0100

    zad 4
    Obrazy w skali szarości można interpretować jako macierze, czyli tablice 2D, gdzie poszczególne piksele to liczby z zakresu [0, 255] (0 - czarny, 255 - biały). Na obrazach można wykonywać operacje takie jak obrót w pionie, obrót w poziomie, obrót o 90 stopni czy odbicie kolorów.
    
    Napisz program, który na początek przyjmuje od użytkownika liczby n oraz m. Program powinien stworzyć macierz ("obraz") rozmiaru n x m, tj. listę list mającą n wierszy i m kolumn, z wartościami losowymi z zakresu [0, 255].
    Przykładowo:
    n = 3
    m = 3
    matrix = [[0, 1, 2],
              [2, 3, 3],
              [4, 1, 4]]
    
    Następnie program powinien przyjmować od użytkownika serię komend. Mogą być to: "flip horizontal", "flip vertical", "rotate left", "rotate right", "reverse values". Użytkownik kończy serię komend wpisując "done".
    Program powinien wykonać po kolei komendy na macierzy:
    "flip horizontal" - odbicie w osi poziomej:
    matrix = [[4, 1, 4],
              [2, 3, 3],
              [0, 1, 2]]
    "flip vertical" - odbicie w osi pionowej:
    matrix = [[2, 0, 1],
              [3, 3, 2],
              [4, 1, 4]]
    "rotate left" - obrót o 90 stopni w lewo:
    matrix = [[4, 2, 0],
              [1, 3, 1],
              [4, 3, 2]]
    
    
    "rotate right" - obrót o 90 stopni w prawo:
    matrix = [[2, 3, 4],
              [1, 3, 1],
              [0, 2, 4]]
    
    
    "reverse values" - interpretujemy wartości jako odcienie szarości, tj. 0 to czarny, a 255 to biały; odbicie takiego obrazu to zamiana 0 na 255, 255 na 0, 1 na 254, 254 na 1 i tak dalej:
    matrix = [[255, 254, 253],
              [253, 252, 252],
              [251, 254, 251]]
    
    Na koniec macierz po wykonaniu wszystkich komend powinna zostać wypisana.

---
+++ ćwiczenia 1/zadania domowe/biblioteka_zadanie_4.py
@@ -0,0 +1,72 @@
+from random import randint
+
+def wypisanie_array(array):
+    for line in array:
+        print(line)
+    print('\n')
+
+def tworzenie_array(n,m):
+    array = []
+    for i in range(n):
+        wiersz = [randint(0,255) for _ in range(m)]
+        array.append(wiersz)
+    return array
+
+def flip_horizontal(array):
+    array_flip_horizontal = array[::-1]
+    print('flip_horizontal')
+    wypisanie_array(array_flip_horizontal)
+    return array_flip_horizontal
+
+def flip_vertical(array):
+    array_flip_vertical = []
+    for line in array:
+        wiersz = line[::-1]
+        array_flip_vertical.append(wiersz)
+
+    print('flip_vertical')
+    wypisanie_array(array_flip_vertical)
+    return array_flip_vertical
+
+def rotate(array,l_obrotow,n,m):
+    if l_obrotow == 1:
+        kierunek ='right'
+    else:
+        kierunek = 'left'
+    while l_obrotow >=1:
+        array_help = []
+        for i in range(m):
+            wiersz = [array[j][i] for j in range(n-1,-1,-1)]
+            array_help.append(wiersz)
+        array = array_help
+        n , m = m , n
+        l_obrotow -= 1
+
+    print('rotate ',kierunek)
+    wypisanie_array(array_help)
+    return array_help,n,m
+
+def reverse_array(array):
+    array_help = []
+    for line in array:
+        wiersz = [255-digit for digit in line]
+        array_help.append(wiersz)
+
+    print('reverse')
+    wypisanie_array(array_help)
+    return array_help
+
+def commandOpenDef(list,array,n,m):#flip_horizontal  ,   flip_vertical   ,  rotate_right   ,  rotate_left   ,   reverse_values
+    for command in list:
+        if command.lower() == 'flip_horizontal':
+            array = flip_horizontal(array)
+        elif command.lower() == 'flip_vertical':
+            array = flip_vertical(array)
+        elif command.lower() == 'rotate_right':
+            array,n,m = rotate(array,1,n,m)
+        elif command.lower() == 'rotate_left':
+            array,n,m = rotate(array,3,n,m)
+        elif command.lower() == 'reverse_values':
+            array = reverse_array(array)
+        else:
+            print('komenda nie zrozumiała: ',command)
---
+++ ćwiczenia 1/zadania domowe/zad 4.py
@@ -0,0 +1,15 @@
+from biblioteka_zadanie_4 import *
+
+if __name__ == '__main__':
+
+    n = int(input('podaj użytkowniku liczbe wierszy macierzy : '))
+    m = int(input('podaj użytkowniku liczbe kolumn macierzy : '))
+
+    array = tworzenie_array(n,m)
+    wypisanie_array(array)
+
+    command = input('>>podaj liste komend po koleji odzielone pomiędzy spacją<<\n'
+                    '>>flip_horizontal  ,   flip_vertical   ,  rotate_right   ,  rotate_left   ,   reverse_values<<\n'
+                    '>>')
+    commandList = command.split()
+    commandOpenDef(commandList,array,n,m)
