150995c3bf2c2735ee3cfd5f9a7aaf06ef9cb517
Krzysiek <krzysiekkondys@gmail.com>
2021-11-16 18:51:33 +0100

    .

---
+++ lesson_6/task/task_1.py
@@ -0,0 +1,19 @@
+#imperatywnie
+# def main():
+#     lista = [ i+1 for i in range(10)]
+#     suma = 0
+#     for i in lista:
+#         suma += i
+#
+#     print(suma)
+#
+# if __name__ == "__main__":
+#     main()
+
+#funkcyjnie
+def main():
+    lista = [i+1 for i in range(10)]
+    print(sum(lista))
+
+if __name__ == "__main__":
+    main()
